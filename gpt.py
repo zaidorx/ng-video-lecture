9ed15d3d767e3e0bb1889638622fa0d1423a2fda
robbinc91 <robbinc91@gmail.com>
2023-01-22 00:42:22 -0500

    added tiktoker tokenizer

--- gpt.py
+++ gpt.py
@@ -2,10 +2,12 @@ import os
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import tiktoken
+
 
 # hyperparameters
 batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
+block_size = 8 # what is the maximum context length for predictions?
 max_iters = 10000
 eval_interval = 200
 learning_rate = 3e-4
@@ -13,17 +15,17 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 100
 n_embd = 384
 n_head = 6
-n_layer = 10
+n_layer = 5
 dropout = 0.2
-predicting = False # set to true to not train on this run
-model_name = "marti.tar"
-best_model_name = "marti_best.tar"
+predicting = True # set to true to not train on this run
+model_name = "shakespeare.tar"
+best_model_name = "shakespeare_best.tar"
 # ------------
 
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('marti.txt', 'r', encoding='utf-8') as f:
+with open('shakespeare.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
@@ -32,11 +34,15 @@ vocab_size = len(chars)
 # create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+#encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+#decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+
+enc = tiktoken.get_encoding('gpt2')
+vocab_size = enc.n_vocab
 
 # Train and test splits
-data = torch.tensor(encode(text), dtype=torch.long)
+data = torch.tensor(enc.encode(text), dtype=torch.long)
 n = int(0.9*len(data)) # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
@@ -52,8 +58,8 @@ def get_batch(split):
     return x, y
 
 def get_initial_tokens(text):
-    data = torch.tensor(encode(text), dtype=torch.long)
-    x = torch.stack([data[i:i+block_size] for i in len(data)])
+    data = torch.tensor(enc.encode(text), dtype=torch.long)
+    x = torch.stack([data[i:i+block_size] for i in range(len(data)-block_size)])
     return x
 
 
@@ -240,12 +246,12 @@ if (epoch_init < max_iters and predicting == False): # continue training
                 model_name)
 # generate from the model
 print("Making predictions....")
-start_text = encode("Mañana ")
+start_text = enc.encode("One ")
 data = torch.tensor(start_text, dtype=torch.long, device=device)
 data = torch.reshape(data, (1,len(data)))
 print(data.shape)
 #context = torch.zeros((1, 1), dtype=torch.long, device=device)
 results = m.generate(data, max_new_tokens=101)
-decoded = decode(results[0].tolist())
+decoded = enc.decode(results[0].tolist())
 print(decoded)
-#open('marti_more.txt', 'w').write(decode(m.generate(data, max_new_tokens=10000)[0].tolist()))
+#open('shakespeare_more.txt', 'w').write(enc.decode(m.generate(data, max_new_tokens=10000)[0].tolist()))
